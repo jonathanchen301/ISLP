d250559132614b55d34f31df6ae795ab3b054cca
Jonathan <jonathanchen301@gmail.com>
2025-05-19 22:08:14 -0400

    Implemention of 3.1.2 Assessing Accuracy of the Coefficient Estimates

--- linear_regression/least_squares.py
+++ linear_regression/least_squares.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+"""
+Estimating Coefficients of Simple Linear Regression.
+"""
+
 def residual(actual, pred):
     """
     Calculate residual between actual values and observed values
@@ -9,7 +13,7 @@ def residual(actual, pred):
     - pred (np array): the predicted values
 
     Returns:
-    - residual (np array): the residual
+    - Residual (np array): the residual
     """
 
     return actual - pred
@@ -23,7 +27,7 @@ def residual_sum_of_squares(actual, pred):
     - pred (np array): the predicted values
 
     Returns: 
-    - residual sum of squares (float): the reesidual sum of squares 
+    - Residual sum of squares (float): the reesidual sum of squares 
     """
 
     return np.sum((actual - pred)**2)
@@ -35,8 +39,8 @@ def least_squares_coefficient(X, Y):
     Y = beta0 + beta1X
 
     Params:
-    - X (np array)
-    - Y (np array)
+    - X (np array): variable
+    - Y (np array): target
 
     Returns:
     - beta0, beta1 (np array)
@@ -51,3 +55,122 @@ def least_squares_coefficient(X, Y):
     beta0 = Y_mean - beta1 * X_mean
 
     return beta0, beta1
+
+"""
+Evaluating Accuracy of the Coefficient Estimates 
+"""
+
+def standard_error(sd, n):
+    """
+    Calculate the standard error of a mean.
+
+    Params:
+    - sd (float): sample standard deviation
+    - sample size (int): sample size
+    
+    Returns:
+    - Standard error of the mean (float)
+    """
+
+    return sd / np.sqrt(n)
+
+# TODO: in later chapters, going to have to generalize to higher degrees of freedoms.
+def residual_standard_error(actual, pred):
+    """
+    Calculate the residual standard error (RSE).
+    
+    Params:
+    - actual (np array): the true values
+    - pred (np array): the predicted values
+
+    Returns:
+    - Residual standard error (float): the residual standard error (RSE)
+    """
+
+    n = actual.shape[0]
+    return np.sqrt(residual_sum_of_squares(actual, pred) / (n - 2))
+
+def least_squares_predict(X, Y):
+
+    """
+    Calculate the predicted values using least squares.
+
+    Params:
+    - X (np array): variable
+    - Y (np array): target
+
+    Returns:
+    - Predicted values (np array)
+    """
+
+    beta0, beta1 = least_squares_coefficient(X, Y)
+    return beta0 + beta1 * X
+
+def standard_error_of_slope(X, Y):
+    """
+    Calculate the standard error of the slope.
+    
+    Params:
+    - X (np array): Variable
+    - Y (np array): Target
+    
+    Returns:
+    - Standard error of the slope predicted using least squares (float)
+    """
+
+    pred = least_squares_predict(X, Y)
+    rse = residual_standard_error(Y, pred)
+    return np.sqrt((rse ** 2) / np.sum((X - np.mean(X)) ** 2))
+
+def standard_error_of_intercept(X, Y):
+    """
+    Calculate the standard error of the intercept.
+    
+    Params:
+    - X (np array): Variable
+    - Y (np array): Target
+    
+    Returns:
+    - Standard error of the intercept predicted using least squares (float)
+    """
+
+    pred = least_squares_predict(X, Y)
+    rse = residual_standard_error(Y, pred)
+    return np.sqrt(((1 / X.shape[0]) + ((np.mean(X) ** 2) / np.sum((X - np.mean(X)) ** 2))) * rse ** 2)
+
+def confidence_interval_95(X, Y):
+    """
+    Calculate the 95% confidence interval for the slope and intercept.
+    
+    Params:
+    - X (np array): Variable
+    - Y (np array): Target
+    
+    Returns:
+    - Confidence interval for beta0 (tuple)
+    - Confidence interval for beta1 (tuple)
+    """
+
+    beta0, beta1 = least_squares_coefficient(X, Y)
+    se_beta0 = standard_error_of_intercept(X, Y)
+    se_beta1 = standard_error_of_slope(X, Y)
+
+    return (beta0 - 2 * se_beta0, beta0 + 2 * se_beta0), (beta1 - 2 * se_beta1, beta1 + 2 * se_beta1)
+
+def t_statistic(X, Y):
+    """
+    Calculate the t-statistic for the slope and intercept.
+
+    It is the number of standard deviations the predicted beta1 is away from 0.
+    
+    Params:
+    - X (np array): Variable
+    - Y (np array): Target
+    
+    Returns:
+    - t-statistic for beta1 (float)
+    """
+
+    _, beta1 = least_squares_coefficient(X, Y)
+    se_beta1 = standard_error_of_slope(X, Y)
+    return beta1 / se_beta1
